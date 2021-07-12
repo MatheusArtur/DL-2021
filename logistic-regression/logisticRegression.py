3fa7dd521acfee258908149c4b0c4403c684e68e
Matheus Costa <matheus@tidy.com>
2021-07-11 21:36:10 -0300

    helper functions

--- logistic-regression/logisticRegression.py
+++ logistic-regression/logisticRegression.py
@@ -34,3 +34,107 @@ print ("train_set_y shape: " + str(train_set_y.shape))
 print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
 print ("test_set_y shape: " + str(test_set_y.shape))
 print ("sanity check after reshaping: " + str(train_set_x_flatten[0:5,0]))
+
+# Standardize Dataset
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
+
+# Helper functions
+def sigmoid(z):
+  s = 1 / (1 + np.exp(-z))
+
+  return s
+
+print ("sigmoid([0, 2]) = " + str(sigmoid(np.array([0,2]))))
+
+# Initial Params
+def initialize_with_zeros(dim):
+    w = np.zeros([dim,1])
+    b = 0
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+
+    return w, b
+
+dim = 2
+w, b = initialize_with_zeros(dim)
+print ("w = " + str(w))
+print ("b = " + str(b))
+
+# Forward and Backward propagation
+def propagate(w, b, X, Y):
+    m = X.shape[1]
+    A = sigmoid(np.dot(w.T,X) + b)
+    dw = 1 / m *(np.dot(X,(A - Y).T))
+    db = 1 / m *(np.sum(A - Y))
+    cost = -1/m * (np.dot(Y,np.log(A).T) + np.dot((1-Y),np.log(1 - A).T))
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return grads, cost
+
+w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
+grads, cost = propagate(w, b, X, Y)
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+print ("cost = " + str(cost))
+
+# Optimization
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    costs = []
+    for i in range(num_iterations):
+        grads, cost = propagate(w,b,X,Y)
+
+        dw = grads["dw"]
+        db = grads["db"]
+
+        w -= learning_rate*dw
+        b -= learning_rate*db
+
+        if i % 100 == 0:
+            costs.append(cost)
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+
+    params = {"w": w,
+              "b": b}
+    grads = {"dw": dw,
+             "db": db}
+
+    return params, grads, costs
+
+params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+print ("w = " + str(params["w"]))
+print ("b = " + str(params["b"]))
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+
+# GRADED FUNCTION: predict
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)
+    A = sigmoid(np.dot(w.T,X) + b)
+
+    for i in range(A.shape[1]):
+        if A[0][i] > 0.5:
+            Y_prediction[0][i] = 1
+        else:
+            Y_prediction[0][i] = 0
+        pass
+
+    Y_prediction[A > 0.5] = 1.
+    assert(Y_prediction.shape == (1, m))
+
+    return Y_prediction
+
+w = np.array([[0.1124579],[0.23106775]])
+b = -0.3
+X = np.array([[1.,-1.1,-3.2],[1.2,2.,0.1]])
+print ("predictions = " + str(predict(w, b, X)))
