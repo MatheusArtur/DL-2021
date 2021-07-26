93bc4bd23e885633377b511f80edae12bf327334
Matheus Costa <matheus@tidy.com>
2021-07-25 22:36:29 -0300

    L Model & Update params

--- deep-neural-network/neural-network.py
+++ deep-neural-network/neural-network.py
@@ -1,7 +1,7 @@
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
-from testCases_v4 import *
+from testCases_v4a import *
 from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
 
 plt.rcParams['figure.figsize'] = (5.0, 4.0)
@@ -52,7 +52,7 @@ def initialize_parameters_deep(layer_dims):
     assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
     assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
 
-      
+
   return parameters
 
 parameters = initialize_parameters_deep([5,4,3])
@@ -68,7 +68,7 @@ def linear_forward(A, W, b):
 
   assert(Z.shape == (W.shape[0], A.shape[1]))
   cache = (A, W, b)
-  
+
   return Z, cache
 
 A, W, b = linear_forward_test_case()
@@ -89,7 +89,7 @@ def linear_activation_forward(A_prev, W, b, activation):
   elif activation == "relu":
       Z, linear_cache = linear_forward(A_prev, W, b)
       A, activation_cache = relu(Z)
-  
+
   assert (A.shape == (W.shape[0], A_prev.shape[1]))
   cache = (linear_cache, activation_cache)
 
@@ -124,7 +124,7 @@ def L_model_forward(X, parameters):
   caches.append(cache)
 
   assert(AL.shape == (1,X.shape[1]))
-          
+
   return AL, caches
 
 X, parameters = L_model_forward_test_case_2hidden()
@@ -157,21 +157,17 @@ def linear_backward(dZ, cache):
   A_prev, W, b = cache
   m = A_prev.shape[1]
 
-  ### START CODE HERE ### (≈ 3 lines of code)
   dW = (1. / m) * (np.dot(dZ, A_prev.T))
   db = (1. / m) * (np.sum(dZ, axis = 1, keepdims=True))
   dA_prev = np.dot(W.T, dZ)
-  ### END CODE HERE ###
-  
+
   assert(dA_prev.shape == A_prev.shape)
   assert(dW.shape == W.shape)
   assert(db.shape == b.shape)
-  
+
   return dA_prev, dW, db
 
-
 dZ, linear_cache = linear_backward_test_case()
-
 print("linear_cache size = ", len(linear_cache) , '\n\n')
 
 print('A^[0] = \n', linear_cache[0] , '\n\n')
@@ -179,26 +175,23 @@ print('W^[1] = \n', linear_cache[1] , '\n\n')
 print('b^[1] = \n', linear_cache[2] , '\n\n')
 
 dA_prev, dW, db = linear_backward(dZ, linear_cache)
-# ??????????????????????
 print ("dA_prev = "+ str(dA_prev))
-# ??????????????????????
 print ("dW = " + str(dW))
-# ??????????????????????
 print ("db = " + str(db))
 
 
-# Linear activation backward
+# Linear Activation backward
 def linear_activation_backward(dA, cache, activation):
   linear_cache, activation_cache = cache
-  
+
   if activation == "relu":
     dZ = relu_backward(dA, activation_cache)
     dA_prev, dW, db = linear_backward(dZ, linear_cache)
-      
+
   elif activation == "sigmoid":
     dZ = sigmoid_backward(dA, activation_cache)
     dA_prev, dW, db = linear_backward(dZ, linear_cache)
-  
+
   return dA_prev, dW, db
 
 dAL, linear_activation_cache = linear_activation_backward_test_case()
@@ -219,3 +212,46 @@ print ("relu:")
 print ("dA_prev = "+ str(dA_prev))
 print ("dW = " + str(dW))
 print ("db = " + str(db))
+
+# L-Model Backward
+def L_model_backward(AL, Y, caches):
+  grads = {}
+  L = len(caches)
+  m = AL.shape[1]
+  Y = Y.reshape(AL.shape)
+
+  dAL =- (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+
+  current_cache = caches[L-1]
+  grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL, current_cache, activation = "sigmoid")
+  
+  for l in reversed(range(L-1)):
+    current_cache = caches[l]
+    dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l + 2)],  current_cache, activation = "relu")
+    grads["dA" + str(l + 1)] = dA_prev_temp
+    grads["dW" + str(l + 1)] = dW_temp
+    grads["db" + str(l + 1)] = db_temp
+
+  return grads
+
+AL, Y_assess, caches = L_model_backward_test_case()
+grads = L_model_backward(AL, Y_assess, caches)
+print_grads(grads)
+
+# Update Parameters
+def update_parameters(parameters, grads, learning_rate):
+  L = len(parameters) // 2
+
+  for l in range(L):
+      parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * grads["dW" + str(l + 1)]
+      parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * grads["db" + str(l + 1)]
+
+  return parameters
+
+parameters, grads = update_parameters_test_case()
+parameters = update_parameters(parameters, grads, 0.1)
+
+print ("W1 = "+ str(parameters["W1"]))
+print ("b1 = "+ str(parameters["b1"]))
+print ("W2 = "+ str(parameters["W2"]))
+print ("b2 = "+ str(parameters["b2"]))
